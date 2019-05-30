8023d1cf337000b0dc2429ec9baa7922a601cefb
Daniel Alves <danielalveslima36>
2019-05-30 01:21:25 -0300

    "Criação do modal para realização do cadastro do usuário"

---
+++ src/main/webapp/js/login.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $('.modal').modal({
+        preventScrolling: true
+    });
+});
+
+
+$(document).ready(function(){
+    $('.datepicker').datepicker({
+        i18n: {
+            cancel: 'Cancelar',
+            clear: 'Limpar',
+            done: 'Ok',
+            months: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+            monthsShort: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
+            weekdays: ["Domingo","Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
+            weekdaysShort: ["Dom","Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"],
+            weekdaysAbbrev: ["DOM","SEG", "TER", "QUA", "QUI", "SEX", "SÁB"]
+
+        },
+
+        format: 'dd/mm/yyyy'
+    });
+});
