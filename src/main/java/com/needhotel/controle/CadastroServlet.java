4d5e8d53b1b04708dbf1cb70b5f1a0aed72c5691
Daniel Alves <danielalveslima36>
2019-06-04 22:32:04 -0300

    Implementação do servlet de cadastro em duas etapas

--- src/main/java/com/needhotel/controle/CadastroServlet.java
+++ src/main/java/com/needhotel/controle/CadastroServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.sql.SQLException;
 import java.time.LocalDate;
 
@@ -15,23 +16,31 @@ import java.time.LocalDate;
 public class CadastroServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest req, HttpServletResponse resp){
-        Usuario usuario = new Usuario();
         UsuarioDAOBD usuarioDAOBD = new UsuarioDAOBD();
+        HttpSession session = req.getSession();
 
-        usuario.setNome(req.getParameter("primeiroNome"));
-        usuario.setSobreNome(req.getParameter("sobrenome"));
-        usuario.setCpf(req.getParameter("cpf"));
-        usuario.setTelefone(req.getParameter("telefone"));
-        usuario.setDataNascimento(LocalDate.parse(req.getParameter("nascimento")));
-        usuario.setEmail(req.getParameter("email"));
-        usuario.setSenha(req.getParameter("senha"));
 
-        try {
-            usuarioDAOBD.cadastrarUsuario(usuario);
-        } catch (SQLException e) {
-            e.printStackTrace();
+        if (session.getAttribute("firstRegister") == null){
+            Usuario usuario = new Usuario();
+            usuario.setCpf(req.getParameter("cpf"));
+            usuario.setTelefone(req.getParameter("telefone"));
+            usuario.setNome(req.getParameter("primeiroNome"));
+            usuario.setSobreNome(req.getParameter("sobrenome"));
+            usuario.setDataNascimento(LocalDate.parse(req.getParameter("nascimento")));
+
+            session.setAttribute("firstRegister", usuario);
+        }else {
+            Usuario usuario = (Usuario)session.getAttribute("firstRegister");
+            usuario.setEmail(req.getParameter("email"));
+            usuario.setSenha(req.getParameter("senha"));
+
+            try {
+                usuarioDAOBD.cadastrarUsuario(usuario);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+
         }
-
-
     }
 }
