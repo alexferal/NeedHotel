3f628fc31e59743d1566751c80a8adefffa9285a
Daniel Alves <danielalveslima36>
2019-06-27 21:57:37 -0300

    Criação das tabelas de Imovel e comodidades do imovel

--- scripts/comodidade.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE comodidade(
-     id_imovel VARCHAR(100),
-     recurso VARCHAR(200),
-     CONSTRAINT comodidade_pk PRIMARY KEY(id),
-     CONSTRAINT id_fk FOREIGN KEY(id)
-         REFERENCES imovel(id)
-
-);
--- scripts/usuario.sql
+++ scripts/usuario.sql
@@ -9,3 +9,29 @@ CREATE TABLE usuario(
     CONSTRAINT cpf_pk PRIMARY KEY(cpf)
 );
 
+CREATE TABLE imovel(
+    id VARCHAR(100),
+    proprietario VARCHAR(14),
+    nome VARCHAR(200),
+    rua VARCHAR(200),
+    bairro VARCHAR(100),
+    numero VARCHAR(10),
+    cep VARCHAR(9),
+    cidade VARCHAR(100),
+    estado VARCHAR(100),
+    valor FLOAT,
+    disponibilidade BOOLEAN,
+    CONSTRAINT id_pk PRIMARY KEY(id),
+    CONSTRAINT proprietario_fk FOREIGN KEY(proprietario)
+        REFERENCES usuario(cpf)
+);
+
+CREATE TABLE comodidade(
+    id_imovel VARCHAR(100),
+    recurso VARCHAR(200),
+    CONSTRAINT comodidade_pk PRIMARY KEY(id_imovel),
+    CONSTRAINT id_fk FOREIGN KEY(id_imovel)
+        REFERENCES imovel(id)
+
+);
+
